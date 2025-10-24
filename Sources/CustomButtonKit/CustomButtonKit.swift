8dd1a380e310f793caaf2eb80f85950c21621578
Felipe Elsner Silva <felipeelsnersilva15@gmail.com>
2025-10-24 14:07:40 -0300

    first commit

--- Sources/CustomButtonKit/CustomButtonKit.swift
+++ Sources/CustomButtonKit/CustomButtonKit.swift
@@ -1,2 +1,21 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+
+struct CustomButton: View {
+    let title: String
+    let action: () -> Void
+    
+    init(title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+        }
+    }
+}
